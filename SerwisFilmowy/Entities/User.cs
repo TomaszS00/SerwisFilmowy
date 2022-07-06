dfe8204f0eb74a04a45c25de64d1a69ea9a5ef23
TomaszS00 <73608769+TomaszS00@users.noreply.github.com>
2022-07-06 17:45:48 +0200

    cleaning

--- SerwisFilmowy/Entities/User.cs
+++ SerwisFilmowy/Entities/User.cs
@@ -8,7 +8,6 @@ public class User
     public int Id { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
-    public string? Name { get; set; }
     public int RoleId { get; set; }
     public virtual Role Role { get; set; }
 }
--- SerwisFilmowy/Models/RegisterDto.cs
+++ SerwisFilmowy/Models/RegisterDto.cs
@@ -6,7 +6,5 @@ public class RegisterDto
 {
     public string Email { get; set; }
     public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
-    public string? Name { get; set; }
     public int RoleId { get; set; }
 }
