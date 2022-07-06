146dfbf2684376780543f0fa90d09363f1e8e4ed
TomaszS00 <73608769+TomaszS00@users.noreply.github.com>
2022-07-06 17:34:33 +0200

    add user controller

---
+++ SerwisFilmowy/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SerwisFilmowy.Models;
+using SerwisFilmowy.Services;
+
+namespace SerwisFilmowy.Controllers;
+
+
+[Route("api/user")]
+[ApiController]
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody] RegisterDto registerDto)
+    {
+        _userService.Register(registerDto);
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public ActionResult Login([FromBody] LoginDto dto)
+    {
+        string token = _userService.GenereteJWT(dto);
+        return Ok(token);
+    }
+}
